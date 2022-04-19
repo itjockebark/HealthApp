1516a9582ac01728a59abfeb0284be25421cf861
itjockebark <jockebark@msn.com>
2022-04-19 11:16:29 +0200

    Adds the foundation to Gym Schedule page

--- src/main/java/com/example/nutritiondata/controller/GymScheduleController.java
+++ src/main/java/com/example/nutritiondata/controller/GymScheduleController.java
@@ -1,10 +1,15 @@
 package com.example.nutritiondata.controller;
 
+import com.example.nutritiondata.model.Exercise;
+import com.example.nutritiondata.model.Schedule;
 import com.example.nutritiondata.service.ExerciseService;
+import com.example.nutritiondata.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class GymScheduleController {
@@ -12,8 +17,16 @@ public class GymScheduleController {
     @Autowired
     ExerciseService exerciseService;
 
+    @Autowired
+    ScheduleService scheduleService;
+
     @GetMapping("/gym_schedule")
     public String gymSchedule(Model model) {
+        List<Schedule> schedules = scheduleService.findAll();
+        List<Exercise> exercises = exerciseService.findAll();
+
+        model.addAttribute("schedules", schedules);
+        model.addAttribute("exercises", exercises);
         return "gym_schedule";
     }
 }
